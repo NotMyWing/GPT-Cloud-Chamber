adc0e3abf4f75f2b292e3095a536696c5dd07810
NotMyWing <winwyv@gmail.com>
2025-08-12 00:47:06 +1100

    Update glass

--- src/shaders/glass.fs
+++ src/shaders/glass.fs
@@ -8,7 +8,7 @@ void main(){
   vec3 Nf = faceforward(N, V, N);
   float fres = pow(1.0 - max(dot(V, Nf), 0.0), 5.0);
   vec3 tint = vec3(0.4, 0.6, 0.7) * 0.15;
-  vec3 refl = vec3(0.9);
+  vec3 refl = vec3(0.1);
   vec3 col = mix(tint, refl, fres);
   float spec = pow(max(dot(reflect(-V, Nf), vec3(0.0,0.0,1.0)), 0.0), 16.0);
   col += vec3(spec);
